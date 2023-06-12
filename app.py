fd3c45d2c22127ececc57d23d3403230e3f7efe4
Gain savagez <60669595+akmullane@users.noreply.github.com>
2023-06-12 11:20:11 -0400

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculator')
+def calculate(num1, num2):
+    result = num1 + num2
+    return f"The result of the calculation is: {result}"
+
+if __name__ == '__main__':
+    app.run()
---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero is not allowed"
+
+# Example usage
+result = add(5, 10)
+print(result)
