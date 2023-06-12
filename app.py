73baa191d0f3b9930569ce03446cb1cb59a0ecd8
Gain savagez <60669595+akmullane@users.noreply.github.com>
2023-06-12 14:05:38 -0400

    no app

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+# from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+# # @app.route('/')
+# # def home():
+# #     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def calculate():
-    num1 = int(request.json['num1'])
-    num2 = int(request.json['num2'])
-    result = num1 + num2
-    return jsonify({'result': result})
+# @app.route('/', methods=['POST'])
+# def calculate():
+#     num1 = int(request.json['num1'])
+#     num2 = int(request.json['num2'])
+#     result = num1 + num2
+#     return jsonify({'result': result})
 
-# if __name__ == '__main__':
-#     app.run()
+# # if __name__ == '__main__':
+# #     app.run()
--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 // script.js
-const form = document.getElementById('calculator-form');
-const resultElement = document.getElementById('result');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const num1 = parseInt(form.elements.num1.value);
-  const num2 = parseInt(form.elements.num2.value);
-
-  // Send a POST request to the server
-  fetch('/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ num1, num2 })
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Display the result on the HTML page
-    resultElement.textContent = `Result: ${data.result}`;
-  });
-});
+function calculate() {
+  const num1 = parseInt(document.getElementsByName('num1')[0].value);
+  const num2 = parseInt(document.getElementsByName('num2')[0].value);
+  const result = num1 + num2;
+  document.getElementById('result').textContent = `Result: ${result}`;
+}
