5b9b239d738a51d877233aa47fe85882a593ade9
Gain savagez <60669595+akmullane@users.noreply.github.com>
2023-06-12 14:09:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function calculate() {
   const num1 = parseInt(document.getElementsByName('num1')[0].value);
   const num2 = parseInt(document.getElementsByName('num2')[0].value);
   const result = num1 + num2;
-  document.getElementById('result').textContent = `Result: ${result}`;
+  document.getElementById('result').textContent = `${result}`;
 }
