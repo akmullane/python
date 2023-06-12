b23dafcd323970c2c5eb73b817d67d756116a7f5
Gain savagez <60669595+akmullane@users.noreply.github.com>
2023-06-12 14:12:12 -0400

    fix cross brownser

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // script.js
 function calculate() {
-  const num1 = parseInt(document.getElementsByName('num1')[0].value);
-  const num2 = parseInt(document.getElementsByName('num2')[0].value);
+  const num1Input = document.getElementById('num1');
+  const num2Input = document.getElementById('num2');
+  const resultElement = document.getElementById('result');
+  
+  const num1 = parseInt(num1Input.value);
+  const num2 = parseInt(num2Input.value);
+  
+  if (isNaN(num1) || isNaN(num2)) {
+    resultElement.textContent = 'Invalid input';
+    return;
+  }
+  
   const result = num1 + num2;
-  document.getElementById('result').textContent = `${result}`;
+  resultElement.textContent = `Result: ${result}`;
 }
