84a995a78b8f87700cfc33bf087c7c9f9aa0ebff
Gain savagez <60669595+akmullane@users.noreply.github.com>
2023-06-12 14:20:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// // script.js
+// function calculate() {
+//   const num1 = parseInt(document.getElementsByName('num1')[0].value);
+//   const num2 = parseInt(document.getElementsByName('num2')[0].value);
+//   const result = num1 + num2;
+//   document.getElementById('result').textContent = `${result}`;
+// }
+
+
+
 // script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const calculateBtn = document.getElementById('calculate-btn');
+  calculateBtn.addEventListener('click', calculate);
+});
+
 function calculate() {
   const num1Input = document.getElementById('num1');
   const num2Input = document.getElementById('num2');
